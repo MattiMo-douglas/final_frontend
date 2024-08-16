ef03cdf241861540d33ba92e829ce81573e52886
Riyiike <funkiefabulous003@gmail.com>
2024-08-15 19:59:25 -0700

    adjustpath

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductList from './components/Product';  
-// import NotFound from './components/NotFound';  
 
 const App = () => (
   <Router>
     <div className="App">
       <Routes>
-        <Route path="/products" element={<ProductList />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="/" element={<ProductList />} />
       </Routes>
     </div>
   </Router>
